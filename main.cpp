3da7265e6bb0c7cd3cb5405c464e3fad59462e7a
xxxserafinski126 <115883470+xxxserafinski126@users.noreply.github.com>
2024-04-16 14:25:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     system("cls");
-    std::cout << "Druga część programu" << std::endl;
+    std::cout << "Druga czesc programu" << std::endl;
     std::cout << yellow << "A" << red << "l" << blue << "a" << green << " m" << gray << "a"
         << light_blue << " k" << light_yellow << "o" << cyan << "t" << magenta << "a";
     std::cout << setColor(FOREGROUND_RED | BACKGROUND_GREEN) << "Miauu" << defcolor << " xxzyw";
